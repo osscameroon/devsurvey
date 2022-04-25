7b492550c3cfebb32bb60708692bb0647c372486
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-04-25 16:19:32 +0200

    add function to read yaml file and a function to save fig as img

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,11 @@
 import yaml
 
 def load_yaml(path):
+    print(path)
     with open(path, 'r') as stream:
-        return yaml.load(stream)
+        return yaml.load(stream, Loader=yaml.FullLoader)
 
+
+def save_plot(fig, path):
+    fig.savefig(path, dpi=fig.dpi)
+    print(f"Saved plot to {path}")
