d2ed808d62fac4d21672d3374eb5cee1a6ced788
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-04-25 22:52:30 +0200

    writte function to plot for language and frameworks

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,11 +1,46 @@
 import yaml
+import matplotlib.pyplot as plt
+import matplotlib.colors as pltc
+from config import COLORS
+
+from random import sample
+
+all_colors = [k for k,v in pltc.cnames.items()]
+
 
 def load_yaml(path):
-    print(path)
     with open(path, 'r') as stream:
         return yaml.load(stream, Loader=yaml.FullLoader)
 
 
-def save_plot(fig, path):
-    fig.savefig(path, dpi=fig.dpi)
-    print(f"Saved plot to {path}")
+def plot_fav_languages(datas: list, path: str):
+    languages = []
+    for data in datas:
+        languages.extend(data["fav_languages"])
+
+    plot_data = {language: languages.count(language) for language in languages}
+    colors = [COLORS.get(lang, "red") for lang in plot_data.keys()]
+    fig = plt.figure()
+    plt.bar(list(plot_data.keys()), list(plot_data.values()), color=colors)
+    plt.xticks(rotation=90)
+    plt.xlabel("Languages")
+    plt.ylabel("Number of people using this language")
+    plt.title("Favourite languages")
+    # plt.legend(list(plot_data.values()) , labels=list(plot_data.keys()))
+    plt.savefig(path)
+    return plot_data
+
+
+def plot_fav_frameworks(datas: list, path: str):
+    frameworks = []
+    for data in datas:
+        frameworks.extend(data["fav_frameworks"])
+
+    plot_data = {framework: frameworks.count(framework) for framework in frameworks}
+    values = list(plot_data.values())
+    X = [x/sum(values) for x in values]
+    colors = [COLORS.get(framework, "red") for framework in plot_data.keys()]
+    fig = plt.figure()
+    plt.pie(X, labels=tuple(plot_data.keys()), colors=colors, autopct='%1.0f%%', startangle=90)
+    plt.savefig(path)
+    return plot_data
