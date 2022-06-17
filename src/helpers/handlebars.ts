478c88fcb043b5a05b6780ada7b6908554455170
“Mankavelda” <manjongmanka@gmail.com>
2022-06-17 11:00:17 +0100

    new update on charts

--- src/helpers/handlebars.ts
+++ src/helpers/handlebars.ts
@@ -1,3 +1,27 @@
+import fs from "fs";
+import * as yaml from "js-yaml";
+import path from "path";
+
+let fileContents = fs.readFileSync(path.join( __dirname ,"..","/res/survey.yaml"), "utf8");
+let data: any = yaml.load(fileContents);
+
+export function findingSomething(what : string, item: string){
+  let total = 0;
+  for (var i = 0; i < data["items"].length; i++) {
+    let get_obj: any = Object.values(data["items"][i][what]);
+    for (var j = 0; j < get_obj.length; j++) {
+      if (get_obj[j].toLowerCase() === item.toLowerCase()) {
+        total++;
+      }
+    }
+  }
+  return total;
+}
+
+export function sum ( a : number, b : number ) {
+    return a + b;
+};
+
 export const isLink = (exp: string, got: string): boolean => {
   return exp === got;
 };
@@ -5,20 +29,20 @@ export const isLink = (exp: string, got: string): boolean => {
 export const ifEquals = (a: string, b: string): boolean => {
   return a === b;
 };
-export const displayPagesNumber = (interval: number,current: number, pages: number ): Array<number> => {
+export const displayPagesNumber = (interval: number, current: number, pages: number): Array<number> => {
   let result = [];
-  for (; interval <= (current+ 4) && interval <= pages; interval++){
+  for (; interval <= (current + 4) && interval <= pages; interval++) {
     result.push(interval);
   }
   return result;
 };
 
-export const isLower = (v1: number,v2: number): boolean => {
+export const isLower = (v1: number, v2: number): boolean => {
   return v1 < v2;
 };
 
-export const ifEqual = (a: number, b: number,extra: number): boolean => {
-  return a === (b+extra);
+export const ifEqual = (a: number, b: number, extra: number): boolean => {
+  return a === (b + extra);
 };
 
 export const ifDifferent = (a: number, b: number): boolean => {
@@ -27,15 +51,15 @@ export const ifDifferent = (a: number, b: number): boolean => {
 
 export const constructUrl = (url: string, param: string): string => {
   let data = url.includes("&") ? url.split("&") : url.split("?");
-  for(let i = 0; i< data.length;i++){
-    if(data[i].includes("page")){
-      data[i] = "page="+param;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].includes("page")) {
+      data[i] = "page=" + param;
     }
   }
   return url.includes("&") ? data.join("&") : data.join("?");
 }
 
-export const contains = (a: string, b:string): boolean => {
+export const contains = (a: string, b: string): boolean => {
   return a.includes(b);
 }
 
